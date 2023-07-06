00890966e6eeb937aadef1c53b4f35fdd6b0bd84
NIDAMANURI SAI ADARSH <99207348+Adarsh-N123@users.noreply.github.com>
2023-07-06 18:56:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def run_check():
     print("running process...")
     subprocess.run(['python', 'check.py'])
     print("Scrapping completed")
+    with open('output.json', 'r') as file:
+        data = json.load(file)
     url = 'https://skitter-adaptable-shallot.glitch.me/receive-data'  # Replace with your Node.js server URL
     files = {'file': ('output.json', json.dumps(data), 'application/json')}
     response = requests.post(url, files=files)
