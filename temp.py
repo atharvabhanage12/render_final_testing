c788cb020435a823055b3abbc8f2b8930f8a62b4
Atharva Sachin Bhanage <73925708+atharvabhanage12@users.noreply.github.com>
2024-07-06 21:01:02 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,20 +1,25 @@
 import os
 import time
 import subprocess
+
+# Run the script and capture the output
 result = subprocess.run(['./script.sh'], capture_output=True, text=True)
+
+# Print the captured stdout and stderr
+print("ATHARVA DEBUG: script.sh output")
+print(result.stdout)
+print(result.stderr)
+
 chromium_path = "/opt/render/project/.render/chrome/opt/google/chrome"
-
-
-print(" ATHARVA DEBUG chromium_path")
+print("ATHARVA DEBUG: chromium_path")
 print(chromium_path)
 
 # Add the Chromium path to the PATH environment variable
-os.environ['PATHCHROME'] =  chromium_path
-os.environ["PATH"]+=":"+chromium_path
+os.environ['PATHCHROME'] = chromium_path
+os.environ["PATH"] += ":" + chromium_path
 
-# Verify the updated PATsH
-print("ATHARVA DEBUG checking just")
+# Verify the updated PATH
+print("ATHARVA DEBUG: Updated PATH")
 print(os.environ['PATH'])
 
 time.sleep(4)
-
