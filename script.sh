86b6c729b4fe9818296e19a99b9a197fcd3461dd
Atharva Sachin Bhanage <73925708+atharvabhanage12@users.noreply.github.com>
2024-07-11 17:17:02 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,46 +1,3 @@
-# #!/usr/bin/env bash
-# # exit on error
-# set -o errexit
-
-# STORAGE_DIR=/opt/render/project/.render
-# echo "Running Script.sh .."
-
-# if [[ ! -d $STORAGE_DIR/chrome ]]; then
-#   echo "...Downloading Chrome"
-#   mkdir -p $STORAGE_DIR/chrome
-#   cd $STORAGE_DIR/chrome
-#   wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#   dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
-#   rm ./google-chrome-stable_current_amd64.deb
-#   chmod -R 777 .
-#   chmod -R 777 $STORAGE_DIR/chrome
-#   ls 
-#   cd $HOME/project/src # Make sure we return to where we were
-
-#   # Change permissions to read and write for all users
-#   ls -l . 
-#   chmod -R 777 $STORAGE_DIR/chrome
-#   echo "check Atharva"
-#   echo "$STORAGE_DIR/chrome/opt/google/chrome"  +" Atharva "
-#   echo $PWD +" Atharva "
-#   ls -l $STORAGE_DIR/chrome/ 
-#   echo $PWD +" Atharva "
-
-#   # Output the path to Chromium
-#   echo "check" +" Atharva "
-#   echo $PATH +" Atharva "
-#   ls -l $STORAGE_DIR/chrome/opt/google/chrome 
-
-
-# else
-#   ls -l $STORAGE_DIR/chrome/opt/google/chrome
-#   ls -l $STORAGE_DIR/chrome/
-
-#   echo "...Using Chrome from cache"
-# fi
-
-# echo "Ended Script.sh .."
-
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
@@ -74,14 +31,13 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo $PATH +" Atharva "
   ls -l $STORAGE_DIR/chrome/opt/google/chrome 
 
+
 else
   ls -l $STORAGE_DIR/chrome/opt/google/chrome
   ls -l $STORAGE_DIR/chrome/
+
   echo "...Using Chrome from cache"
 fi
 
-# Check the version of Chrome
-echo "Checking Chrome version..."
-$STORAGE_DIR/chrome/opt/google/chrome/google-chrome --version
-
 echo "Ended Script.sh .."
+
