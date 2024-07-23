b9f638a257cb1a75b6700e64101893a4e1a658af
Atharva Sachin Bhanage <73925708+atharvabhanage12@users.noreply.github.com>
2024-07-23 19:27:30 +0530

    wokring yahoo

--- passed/yahoo.py
+++ passed/yahoo.py
@@ -9,7 +9,6 @@
 # import json
 # from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.chrome.options import Options
-# import requests
 # from bs4 import BeautifulSoup
 
 # # Set up logging configuration
@@ -23,13 +22,16 @@
 # )
 # logger = logging.getLogger(__name__)
 
+# logger.info("Starting script")
+
+# chrome_binary_path = "/opt/render/project/src/chrome/chrome-linux64/chrome"
 # # Set up Chrome options
 # chrome_options = Options()
 # chrome_options.add_argument("--headless")  # Run headless if needed
 # chrome_options.add_argument("--no-sandbox")
 # chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.binary_location = chrome_binary_path
 
-# logger.info("Starting script")
 # # Path to the manually downloaded ChromeDriver
 # chrome_driver_path = os.path.expanduser("/opt/render/project/src/chromedriver/chromedriver-linux64/chromedriver")
 # logger.info(f"ChromeDriver Path: {chrome_driver_path}")
@@ -151,10 +153,9 @@
 #     count += 1
 
 # logger.info("Data collection complete")
-# # Save the data as JSON
+# # Save the data as JSON and log it
 # json_data = json.dumps({"company": "yahoo", "data": L}, indent=4)
-# logger.info("Data saved to JSON")
-# print(json_data)
+# logger.info(f"Data saved to JSON: {json_data}")
 
 # # Quit the driver
 # driver.quit()
@@ -317,9 +318,12 @@ for job in total:
     count += 1
 
 logger.info("Data collection complete")
+
 # Save the data as JSON and log it
-json_data = json.dumps({"company": "yahoo", "data": L}, indent=4)
-logger.info(f"Data saved to JSON: {json_data}")
+output_path = "/opt/render/project/src/output.json"
+with open(output_path, "w") as f:
+    json.dump({"company": "yahoo", "data": L}, f, indent=4)
+logger.info(f"Data saved to JSON: {output_path}")
 
 # Quit the driver
 driver.quit()
