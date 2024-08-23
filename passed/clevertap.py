8ed33b9255e34550736f2b6f9b1479bdfa7b35e4
Atharva Sachin Bhanage <73925708+atharvabhanage12@users.noreply.github.com>
2024-08-23 14:52:06 +0530

    .

--- passed/clevertap.py
+++ passed/clevertap.py
@@ -21,7 +21,7 @@ print(json_data)
 L=json.loads(json_data)
 output_path = "/opt/render/project/src/output1.json"
 with open(output_path, "w") as f:
-    json.dump({"company": "clevertap", "data": L}, f, indent=4)
+    json.dump(json.loads({"company": "clevertap", "data": L}), f, indent=4)
 
 
 
